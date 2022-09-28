9dc505fb14839f14df3a71d32bcf05ac282c88b9
Lucía Lingua <lucia.lingua@mercadolibre.com>
2022-09-28 15:22:00 -0300

    prueba templates

---
+++ lu/nombre_edad/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Person struct {
+	Nombre  string
+	Edad    int32
+	Hobbies []string
+}
+
+var funcs = template.FuncMap{
+	"increment": func(num int) int {
+		return num + 1
+	},
+}
+
+func main() {
+
+	p1 := &Person{"Lucia", 26, []string{"Pintar", "Ir al gym", "Estudiar inglés"}}
+
+	loadTemplate("template2.txt", p1)
+
+}
+
+func loadTemplate(fileName string, data interface{}) {
+	t, err := template.New(fileName).Funcs(funcs).ParseFiles("templates/" + fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+
+}
