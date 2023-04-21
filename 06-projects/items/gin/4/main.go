499ccb88fdcf31035881ae73b147e9c79c6f1581
pacristomeli <ext_pacristo@mercadolibre.com>
2023-04-21 09:02:17 -0300

    items 4

--- 06-projects/items/gin/4/main.go
+++ 06-projects/items/gin/4/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,12 +19,16 @@ func main() {
 
 	r := newRepository()
 
+	u := newitemUsecase(r)
+
 	// creacion instacia del handler
 	// es necesario inyectar en newHandler un repositorio
-	h := newHandler(r)
+	h := newHandler(u)
 
 	// Se definen las rutas
 	router.GET("/", h.helloWorld)
+	router.POST("/items", h.saveItemHandler)
+	router.GET("/items", h.getAllItemsHandler)
 
 	log.Println("Servidor escuchando en http://localhost:8080/")
 
@@ -33,31 +38,64 @@ func main() {
 	}
 }
 
-// ATENCION: solo se implemento un repostorio pero todavia no utiliza en ningun lado
+//////////////////////////////////////////////////////////////////////////////
+// Handler
+//////////////////////////////////////////////////////////////////////////////
 
-// se crea el tipo o type handler
-// con un campo de tipo repository
 type handler struct {
-	repo *repository
+	usecase *itemUsecase
 }
 
 // constructor de typo handler, en los parametros de entrada se inyencta el un repository
-func newHandler(r *repository) *handler {
+func newHandler(u *itemUsecase) *handler {
 	return &handler{
-		repo: r, // aqui se carga el repostory inyectoado dentro del handler
+		usecase: u, // aqui se carga el repostory inyectoado dentro del handler
 	}
 }
 
 // como ahora la antigua funcion helloWorld, tiene un reciber de tipo handler,
 // es un metodo de handler
-func (h handler) helloWorld(c *gin.Context) {
+func (h *handler) helloWorld(c *gin.Context) {
 	c.String(http.StatusOK, "¡Hello World!")
 }
 
-// se añade un repositorio de tipo inmemory
+func (h *handler) saveItemHandler(c *gin.Context) {
+	var item item
+	err := c.BindJSON(&item)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-// entidad Item
-type Item struct {
+	savedItem, err := h.usecase.addItem(item)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, savedItem)
+}
+
+func (h *handler) getAllItemsHandler(c *gin.Context) {
+	items, err := h.usecase.getAllItems()
+	if err != nil {
+		if err == ErrNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, items)
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Repositorio
+/////////////////////////////////////////////////////////////////////////////
+
+// entidad item
+type item struct {
 	ID          int
 	Code        string
 	Title       string
@@ -69,7 +107,7 @@ type Item struct {
 	UpdatedAt   time.Time
 }
 
-type mapRepo map[int]Item
+type mapRepo map[int]item
 
 // creacion del tipo Repository
 type repository struct {
@@ -85,7 +123,7 @@ func newRepository() *repository {
 
 // este metodo sirve para guardar un item en la base de datos
 // este metodo, si bien esta implementado, TODAVIA NO SE UTILIZA
-func (r *repository) saveItem(item Item) error {
+func (r *repository) saveItem(item item) error {
 	if item.ID == 0 {
 		return fmt.Errorf("item ID cannot be 0")
 	}
@@ -94,4 +132,44 @@ func (r *repository) saveItem(item Item) error {
 	}
 	r.items[item.ID] = item
 	return nil
+}
+
+func (r *repository) getAllItems() (mapRepo, error) {
+	return r.items, nil
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Usecases
+/////////////////////////////////////////////////////////////////////////////
+
+var ErrNotFound = errors.New("not found")
+
+type itemUsecase struct {
+	repo *repository
+}
+
+func newitemUsecase(repo *repository) *itemUsecase {
+	return &itemUsecase{
+		repo: repo,
+	}
+}
+func (u *itemUsecase) getAllItems() (mapRepo, error) {
+	items, err := u.repo.getAllItems()
+	if err != nil {
+		return items, fmt.Errorf("error in repository: %w", err)
+	}
+
+	if len(items) == 0 {
+		return items, ErrNotFound
+	}
+
+	return items, nil
+}
+
+func (u *itemUsecase) addItem(item item) (item, error) {
+	if err := u.repo.saveItem(item); err != nil {
+		return item, fmt.Errorf("error saving item: %w", err)
+	}
+
+	return item, nil
 }
