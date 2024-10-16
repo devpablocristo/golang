661fa274ea3f535a0c8d9f6f7a1a81b935521129
devpablocristo <devpablocristo@gmail.com>
2024-10-16 12:36:43 -0300

    sep sdk 2

---
+++ sdk/ciudadanos/auth/go.mod
@@ -0,0 +1,5 @@
+module github.com/devpablocristo/golang/sdk/ciudadanos/auth
+
+go 1.22.3
+
+
--- sdk/databases/in-memory/mapdb/bootstrap.go
+++
@@ -1,9 +0,0 @@
-package sdkmapdb
-
-import (
-	ports "github.com/devpablocristo/golang/sdk/databases/in-memory/mapdb/ports"
-)
-
-func Boostrap() ports.Repository {
-	return newRepository()
-}
